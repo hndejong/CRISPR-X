cdd823ad7d71c7784504a787b9ffb4a7759257a1
Laure Fresard <lfresard@DN0a272363.SUNet>
2017-01-31 15:44:49 -0800

    debug iteration

--- hotspot_analysis/launch_mutationPerRead_hotspots_slidingwindow.sh
+++ hotspot_analysis/launch_mutationPerRead_hotspots_slidingwindow.sh
@@ -11,7 +11,7 @@ BAM_DIR=$6
 chr=$7
 
 
-for i in $(seq {$H1 $H2}); do 
+for i in $(seq $H1 $H2); do 
 	cmd='bash /users/lfresard/CRISPR-X/scripts/mutations_hotspots.sh ${sample} $mismatch $i $(($i+40)) ${WORK_DIR} ${BAM_DIR} ${chr}'
 	echo $cmd >$WORK_DIR/CX${sample}_mutread_hotspot_${i}-${(($i+40))}.log
 	eval $cmd >> $WORK_DIR/CX${sample}_mutread_hotspot_${i}-${(($i+40))}.log 2>&1 &
