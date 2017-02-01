47a023c491ab8c26696c7cb250e7003d2190a231
Laure Fresard <lfresard@DN0a272363.SUNet>
2017-01-31 16:53:41 -0800

    rename log file

--- hotspot_analysis/launch_mutation_reads_summary_slidingwindow.sh
+++ hotspot_analysis/launch_mutation_reads_summary_slidingwindow.sh
@@ -9,5 +9,5 @@ sample=$3
 cmd='Rscript /users/lfresard/repos/crispr-x/hotspot_analysis/mutation_reads_summary_slidingwindow.R ${WORK_DIR} ${parent} ${sample}'
 date >$WORK_DIR/Mutread_hotspot_CX${sample}_slidingwindow_summary.log
 echo $cmd >>$WORK_DIR/Mutread_hotspot_CX${sample}_slidingwindow_summary.log
-eval $cmd >> $WORK_DIR/Mutread_hotspot_CX${sample}_slidignwindow_summary.log 2>&1
+eval $cmd >> $WORK_DIR/Mutread_hotspot_CX${sample}_slidingwindow_summary.log 2>&1
 date >>$WORK_DIR/Mutread_hotspot_CX${sample}_slidingwindow_summary.log
