0696615567cdf34c55cb2125552104605abd4d4d
Laure Fresard <lfresard@DN52eo3a.SUNet>
2017-02-02 11:51:37 -0800

    Change window sizze

--- hotspot_analysis/launch_mutationPerRead_hotspots_slidingwindow.sh
+++ hotspot_analysis/launch_mutationPerRead_hotspots_slidingwindow.sh
@@ -12,7 +12,7 @@ chr=$7
 
 
 for i in $(seq $H1 $H2); do 
-	j=$(($i+40))
+	j=$(($i+100))
 	cmd='bash /users/lfresard/repos/crispr-x/hotspot_analysis/mutations_hotspots_slidingwindow.sh ${sample} $mismatch $i $j ${WORK_DIR} ${BAM_DIR} ${chr}'
 	echo $cmd >$WORK_DIR/CX${sample}_mutread_hotspot_${i}-${j}.log
 	eval $cmd >> $WORK_DIR/CX${sample}_mutread_hotspot_${i}-${j}.log 2>&1 &
