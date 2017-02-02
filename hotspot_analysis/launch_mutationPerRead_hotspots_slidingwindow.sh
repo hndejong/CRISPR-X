c23a587ebc95da25371395a606ca1c5f6b5c752b
Laure Fresard <lfresard@DN52eo3a.SUNet>
2017-02-02 13:27:02 -0800

    launch per read mutation count 1 by 1, clean the script from unused variables

--- hotspot_analysis/launch_mutationPerRead_hotspots_slidingwindow.sh
+++ hotspot_analysis/launch_mutationPerRead_hotspots_slidingwindow.sh
@@ -15,6 +15,6 @@ for i in $(seq $H1 $H2); do
 	j=$(($i+100))
 	cmd='bash /users/lfresard/repos/crispr-x/hotspot_analysis/mutations_hotspots_slidingwindow.sh ${sample} $mismatch $i $j ${WORK_DIR} ${BAM_DIR} ${chr}'
 	echo $cmd >$WORK_DIR/CX${sample}_mutread_hotspot_${i}-${j}.log
-	eval $cmd >> $WORK_DIR/CX${sample}_mutread_hotspot_${i}-${j}.log 2>&1 &
+	eval $cmd >> $WORK_DIR/CX${sample}_mutread_hotspot_${i}-${j}.log 2>&1
 
 done
