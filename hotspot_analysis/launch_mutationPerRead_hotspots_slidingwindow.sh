c4f8290d4ad16a9ebad000a4e8efc31597653a94
Laure Fresard <lfresard@DN52eoiq.SUNet>
2017-02-17 08:53:27 -0800

    modify scripts to tune window size and choose between strict and soft modes

--- hotspot_analysis/launch_mutationPerRead_hotspots_slidingwindow.sh
+++ hotspot_analysis/launch_mutationPerRead_hotspots_slidingwindow.sh
@@ -9,11 +9,12 @@ H2=$4
 WORK_DIR=$5
 BAM_DIR=$6
 chr=$7
-
+window_size=$8
+mode=$9
 
 for i in $(seq $H1 $H2); do 
-	j=$(($i+100))
-	cmd='bash /users/lfresard/repos/crispr-x/hotspot_analysis/mutations_hotspots_slidingwindow.sh ${sample} $mismatch $i $j ${WORK_DIR} ${BAM_DIR} ${chr}'
+	j=$(($i+$window_size))
+	cmd='bash /users/lfresard/repos/crispr-x/hotspot_analysis/mutations_hotspots_slidingwindow.sh ${sample} $mismatch $i $j ${WORK_DIR} ${BAM_DIR} ${chr} ${mode}'
 	echo $cmd >$WORK_DIR/CX${sample}_mutread_hotspot_${i}-${j}.log
 	eval $cmd >> $WORK_DIR/CX${sample}_mutread_hotspot_${i}-${j}.log 2>&1
 
