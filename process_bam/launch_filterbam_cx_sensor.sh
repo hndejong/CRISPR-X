f35264aeed7ccb0ec4e596da123b396d551d60b2
Laure Fresard <lfresard@DN52eobc.SUNet>
2017-08-02 08:42:22 -0700

    add scripts to filter for mapping quality bam files provided by Gaelen

--- process_bam/launch_filterbam_cx_sensor.sh
+++ process_bam/launch_filterbam_cx_sensor.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 #LF
-#Launch Filters on Bam form a specified directory
+#Launch Filters on BAM file form a specified directory
 
-SCRIPT_DIR=/users/lfresard/CRISPR-X/scripts
+filter_script=/users/lfresard/repos/crispr-x/process_bam/Filterbam_MAPQ_cx_sensor.sh
 BAM_DIR=$1
 OUT_DIR=$2
 REF=$3
-n1=$4
-n2=$5
-mismatch=$6
-for i in $(seq $n1 $n2) ; do
-	cmd="bash $SCRIPT_DIR/Filterbam_MAPQ.sh $BAM_DIR $OUT_DIR $REF ${i} ${mismatch}"
-	echo $cmd
-	eval $cmd &
-	
-done
 
+cd $BAM_DIR
+
+ls *.bam | sed 's/.bam/\t/' | awk '{print $1}' | awk -v BAM_DIR=$BAM_DIR -v OUT_DIR=$OUT_DIR -v REF=$REF 'BEGIN{OFS="\t"}{print $1".bam", OUT_DIR"/"$1"_mapq30.bam", REF}' |
+	parallel --jobs 10 --col-sep "\t" "${filter_script} {1} {2} {3}"
