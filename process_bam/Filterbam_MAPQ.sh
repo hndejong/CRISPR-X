2e69607665f0780d6de148409468443400621129
lfresard <lfresard@users.noreply.github.com>
2019-07-16 14:12:00 -0700

    Update Filterbam_MAPQ.sh

--- process_bam/Filterbam_MAPQ.sh
+++ process_bam/Filterbam_MAPQ.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 #LF
-#1/Filter for MAPQ>30
-#2/Filter for PCR duplicates
+#Filter for MAPQ>30. 
+#Input example: CX1_n5.bam
+# Output name example: CX1_n5_mapq30_sorted.bam. This will be the input for samtools mpileup in the next step.
 
 date
 
@@ -17,8 +18,8 @@ mapq_sorted=$(basename $mapq_suf .bam)_sorted
 
 cd $OUT_DIR
 #1/ MAPQ30
-samtools view -h $WORK_DIR/$bam_file | awk '($5>=30)||($1=="@SQ")'| samtools view -Sbh - > $OUT_DIR/$mapq_suf
-samtools sort -m 10G $mapq_suf $mapq_sorted
-samtools index $mapq_sorted.bam
+samtools view -h $WORK_DIR/$bam_file | awk '($5>=30)||($1=="@SQ")'| samtools view -Sbh - > $OUT_DIR/$mapq_suf #filter for mapping quality 
+samtools sort -m 10G $mapq_suf $mapq_sorted # sort filtered bam
+samtools index $mapq_sorted.bam # index filtered bam
 
 cd -
