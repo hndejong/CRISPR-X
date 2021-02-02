28e540e781639e2d036b1d818e4778f178c216e4
Hannah De Jong <hndejong@users.noreply.github.com>
2021-02-01 19:11:29 -0800

    Change hardcoded source code directory to user option

--- Get_variations/launch_mpileupanalysis.sh
+++ Get_variations/launch_mpileupanalysis.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-REF=$1
-BAM_DIR=$2
-OUT_DIR=$3
-mismatch=$4
-N1=$5
-N2=$6
-QUAL=$7
+DIR=$1
+REF=$2
+BAM_DIR=$3
+OUT_DIR=$4
+mismatch=$5
+N1=$6
+N2=$7
+QUAL=$8
 
-
-SCRIPT_DIR=/users/lfresard/CRISPR-X/scripts
-
-for i in `seq $N1 $N2`; do
-	bash $SCRIPT_DIR/mpileup_analysis.sh $REF $BAM_DIR $OUT_DIR $mismatch $i $QUAL &
+for i in `seq $N1 $N2`; 
+do
+	bash $DIR/Get_variations/mpileup_analysis.sh $DIR $REF $BAM_DIR $OUT_DIR $mismatch $i $QUAL &
 done
