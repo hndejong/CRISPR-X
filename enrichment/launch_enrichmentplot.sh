20d2bf8a39634d3b21591ac858c0656fad0b1464
Hannah De Jong <hndejong@users.noreply.github.com>
2021-02-01 19:13:52 -0800

    Change hardcoded source code directory to user option

--- enrichment/launch_enrichmentplot.sh
+++ enrichment/launch_enrichmentplot.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-mut_n1=$1
-mut_n2=$2
-mismatch=$3
-cont_file=$4
-chr=$5
-dir=$6
+
+DIR=$1
+mut_n1=$2
+mut_n2=$3
+mismatch=$4
+cont_file=$5
+chr=$6
+dir=$7
+count_dir=$8
 
 log=CX${mut_n1}_${mut_n2}_${chr}_enrich.log
 
 for i in `seq $mut_n1 $mut_n2` ; do
-	cmd="Rscript scripts/mpileupcounts_plotter_enrichmentonly.R CX${i}_n${mismatch}_mapq30_sorted_qual30.count $cont_file $chr $dir"
+	cmd="Rscript $DIR/enrichment/mpileupcounts_plotter_enrichmentonly.R $count_dir/CX${i}_n${mismatch}_mapq30_sorted_qual30.count $cont_file $chr $dir"
 	echo $cmd >>$dir/$log
 	eval $cmd >>$dir/$log 2>&1
 done
-
